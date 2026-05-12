fc39f89d2a27b63fae750130910404580dff8c38
Tommy Tsang <tsangtakyun@gmail.com>
2026-05-12 10:42:51 +0100

    Apply SOON design system

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh-Hant">
+      <head>
+        <link rel="stylesheet" href="/soon-design-system.css" />
+      </head>
       <body>{children}</body>
     </html>
   )
